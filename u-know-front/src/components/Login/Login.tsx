55802eea49ec6c7c8cbc9b3fd68fc1c009786a44
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-07-26 17:28:22 +0200

    login

---
+++ u-know-front/src/components/Login/Login.tsx
@@ -0,0 +1,51 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {ChangeEvent, FormEvent, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { authService } from '../../services/auth.service';
+
+export default function Login() {
+
+    const navigate = useNavigate()
+    const initialState = { email:'', password:''}
+
+    const [formData, setFromData] = useState({email:'', password:'',})
+    const handleChange =(event:ChangeEvent<HTMLInputElement>)=>{
+        setFromData({
+            ...formData,
+            [event.target.name]: event.target.value 
+        })
+    }
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();  
+        console.log(formData)
+         const response = await authService.login(formData)
+       localStorage.setItem('token', response.data.access_token)
+       navigate('/home') 
+       setFromData(initialState)  
+    }
+
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control name='email' value={formData.email} onChange={handleChange} type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control name='password' value={formData.password} onChange={handleChange} type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
--- u-know-front/src/pages/LoginPage.tsx
+++ u-know-front/src/pages/LoginPage.tsx
@@ -1,3 +1,7 @@
+import Login from "../components/Login/Login";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return <div>
+    <Login/>
+    </div>;
 }
