6bd0e08a15387f6bc65bbc6e70a1adff84799fd7
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-07-26 17:58:27 +0200

    profile

--- u-know-front/src/components/Header/Header.tsx
+++ u-know-front/src/components/Header/Header.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
 import { FaWallet } from 'react-icons/fa';
-import UserAvatar from '../UserAvatar/UserAvatar';
 
 const Header: React.FC = () => {
-    const avatarUrl = 'URL_DEL_AVATAR_DEL_USUARIO';
-    const username = 'NOMBRE_DEL_USUARIO';
+
     return (
         <header>
             <div className="logo">
@@ -23,7 +21,7 @@ const Header: React.FC = () => {
                     </li>
                     <li>
                         <Link to="/profile">
-                            <UserAvatar avatarUrl={avatarUrl} username={username} />
+                            
                         </Link>
                     </li>
                 </ul>
