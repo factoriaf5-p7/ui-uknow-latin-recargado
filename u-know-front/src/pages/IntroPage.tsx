7ef7feba055dbffc9bee2ae771f99de9bd97baef
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-07-27 17:35:48 +0200

    solucion temporal para hero

--- u-know-front/src/pages/IntroPage.tsx
+++ u-know-front/src/pages/IntroPage.tsx
@@ -1,12 +1,36 @@
-import { Button } from "react-bootstrap";
-import './StylePages/IntroPage.css'
-import { Link } from "react-router-dom";
-export default function IntroPage() {
-  return <>
-  <Link to="/home">
-  <Button variant="primary" type="submit">
-Go!
-</Button>
-</Link>
-</>;
-}
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+import { BiSearch } from 'react-icons/bi';
+import './IntroPage.css';
+import heroImage from '../assets/fondo-uk.png';
+
+const IntroPage: React.FC = () => {
+  const [isButtonPressed, setIsButtonPressed] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsButtonPressed(true);
+    setTimeout(() => {
+      setIsButtonPressed(false);
+      window.location.href = '/home';
+    }, 200); // Redirigimos a la página de inicio después de 200ms (ajusta este valor según tu preferencia)
+  };
+
+  // Configuración de animación con react-spring
+  const springStyle = useSpring({
+    transform: isButtonPressed ? 'scale(0.95)' : 'scale(1)',
+  });
+
+  return (
+    <div className="hero-section" style={{ backgroundImage: `url(${heroImage})` }}>
+      <animated.button
+        className="app-button"
+        style={springStyle}
+        onClick={handleButtonClick}
+      >
+        Ver contenidos
+      </animated.button>
+    </div>
+  );
+};
+
+export default IntroPage;
