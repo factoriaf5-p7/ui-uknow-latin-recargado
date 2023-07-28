0d860e40159c7468fa586acad6b2142aa1456393
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-07-28 10:50:05 +0200

    update package

--- u-know-front/src/components/Login/Login.tsx
+++ u-know-front/src/components/Login/Login.tsx
@@ -1,60 +1,93 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import {ChangeEvent, FormEvent, useState} from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { authService } from '../../services/user.service';
-import './Login.css'
+import './Login.css';
 import { useUserContext } from '../Header/UserContext';
-
+import { Image } from 'react-bootstrap';
 
 export default function Login() {
-  const navigate = useNavigate()
-  const initialState = { email:'', password:''}
-  const [formData, setFromData] = useState({ email: '', password: '', })
+  const navigate = useNavigate();
+  const initialState = { email: '', password: '' };
+  const [formData, setFromData] = useState({ email: '', password: '' });
   const { setUserNameAfterLogin } = useUserContext();
 
-    const handleChange =(event:ChangeEvent<HTMLInputElement>)=>{
-        setFromData({
-            ...formData,
-            [event.target.name]: event.target.value 
-        })
-    }
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>)=>{
-        event.preventDefault();  
-        //console.log(formData)
-         const response = await authService.login(formData)
-      localStorage.setItem('token', response.data.access_token)
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFromData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-      setUserNameAfterLogin(formData.email);
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //console.log(formData)
+    const response = await authService.login(formData);
+    localStorage.setItem('token', response.data.access_token);
 
-      localStorage.setItem('name', response.data.name)
-      localStorage.setItem('wallet_balance', response.data.wallet_balance)
-      //console.log(response)
-      
-      setUserNameAfterLogin(response.data.name);
-       navigate('/home-user') 
-       setFromData(initialState)  
-       navigate('/home-user') 
-       setFromData(initialState)  
-    }
+    setUserNameAfterLogin(formData.email);
+
+    localStorage.setItem('name', response.data.name);
+    localStorage.setItem('wallet_balance', response.data.wallet_balance);
+    //console.log(response)
+
+    setUserNameAfterLogin(response.data.name);
+    navigate('/home-user');
+    setFromData(initialState);
+    navigate('/home-user');
+    setFromData(initialState);
+  };
+
+  // Genera un avatar aleatorio con iniciales basado en el nombre del usuario
+  const getRandomAvatar = () => {
+    const initials = formData.email
+      .split('@')[0] // Obtiene el nombre de usuario antes del '@'
+      .split('.')
+      .map((namePart) => namePart.charAt(0).toUpperCase()) // Toma la primera letra de cada parte del nombre y la convierte a mayúscula
+      .join(''); // Une las iniciales
+
+    return <Avatar name={initials} size="80" round />;
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
+      {/* Avatar */}
+      {formData.email ? (
+        getRandomAvatar()
+      ) : (
+        <Image
+          src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp"
+          alt="Avatar"
+          roundedCircle
+          style={{ width: '80px', height: '80px', margin: '0 auto', display: 'block' }}
+        />
+      )}
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control name='email' value={formData.email} onChange={handleChange} type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-        </Form.Text>
+        <Form.Label>Dirección de correo electrónico</Form.Label>
+        <Form.Control
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          type="email"
+          placeholder="Ingresa tu correo electrónico"
+        />
+        <Form.Text className="text-muted"></Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control name='password' value={formData.password} onChange={handleChange} type="password" placeholder="Password" />
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          type="password"
+          placeholder="Contraseña"
+        />
       </Form.Group>
-      
-       <Button variant="primary" type="submit">
-        Login
+
+      <Button variant="primary" type="submit">
+        Iniciar sesión
       </Button>
-      </Form>
+    </Form>
   );
 }
--- u-know-front/src/pages/IntroPage.tsx
+++ u-know-front/src/pages/IntroPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
-/* import { BiSearch } from 'react-icons/bi'; */
 import '../pages/StylePages/IntroPage.css';
 import heroImage from '../assets/fondo-uk.png';
 
@@ -12,10 +11,9 @@ const IntroPage: React.FC = () => {
     setTimeout(() => {
       setIsButtonPressed(false);
       window.location.href = '/home';
-    }, 200); // Redirigimos a la página de inicio después de 200ms (ajusta este valor según tu preferencia)
+    }, 200); // Redirigimos a la página de inicio después de 200ms 
   };
 
-  // Configuración de animación con react-spring
   const springStyle = useSpring({
     transform: isButtonPressed ? 'scale(0.95)' : 'scale(1)',
   });
