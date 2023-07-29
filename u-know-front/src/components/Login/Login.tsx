b2c835e075e80ffe8a3542d2a961b77352be598d
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-07-29 16:59:02 +0200

    alert error login

--- u-know-front/src/components/Login/Login.tsx
+++ u-know-front/src/components/Login/Login.tsx
@@ -1,50 +1,68 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Alert } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { authService } from '../../services/user.service';
 import './Login.css';
 import { useUserContext } from '../Header/UserContext';
- 
 
 export default function Login() {
- const navigate = useNavigate();
+  const navigate = useNavigate();
   const initialState = { email: '', password: '' };
   const [formData, setFromData] = useState({ email: '', password: '' });
+  const [error, setError] = useState<string | null>(null); // Para almacenar el error
   const { setUserNameAfterLogin } = useUserContext();
 
-    const handleChange =(event:ChangeEvent<HTMLInputElement>)=>{
-        setFromData({
-            ...formData,
-            [event.target.name]: event.target.value 
-        })
-    }
-     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-     console.log(formData)
-    const response = await authService.login(formData);
-    localStorage.setItem('token', response.data.access_token);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFromData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-      localStorage.setItem('name', response.data.name)
-      localStorage.setItem('wallet_balance', response.data.wallet_balance)
-      console.log(response)
-      
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError(null); // Pa´ borrar el error antes de intentar iniciar de sesión
+    try {
+      const response = await authService.login(formData);
+      localStorage.setItem('token', response.data.access_token);
+      localStorage.setItem('name', response.data.name);
+      localStorage.setItem('wallet_balance', response.data.wallet_balance);
+      console.log(response);
+
       setUserNameAfterLogin(response.data.name);
-       navigate('/home-user') 
-       setFromData(initialState)    
+      navigate('/home-user');
+      setFromData(initialState);
+    } catch (error) {
+      setError('Oops! Parece que algo salió mal. Revisa tu email y contraseña e inténtalo nuevamente'); // mensaje de error si no se puede iniciar sesión
     }
+  };
 
   return (
-    <Form onSubmit={handleSubmit}>    
+    <Form onSubmit={handleSubmit}>
+      {error && <Alert variant="danger">{error}</Alert>} 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control name='email' value={formData.email} onChange={handleChange} type="email" placeholder="Enter email" required />
-        <Form.Text className="text-muted">
-        </Form.Text>
+        <Form.Control
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          type="email"
+          placeholder="Enter email"
+          required
+        />
+        <Form.Text className="text-muted"></Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control name='password' value={formData.password} onChange={handleChange} type="password" placeholder="Password" required />
+        <Form.Control
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+          required
+        />
       </Form.Group>
 
       <Button variant="primary" type="submit">
@@ -52,4 +70,4 @@ export default function Login() {
       </Button>
     </Form>
   );
-  }
+}
