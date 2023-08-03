d230341cf591fcd19040615c8a7efef6c65b1b91
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-08-03 23:05:22 +0200

    update

--- u-know-front/src/components/Purchased/Purchased.tsx
+++ u-know-front/src/components/Purchased/Purchased.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 const Purchased = () => {
     const [boughtContent, setBoughtContent] = useState<Content[]>([]);
+    const [showAlert, setShowAlert] = useState(false);
 
     useEffect(() => {
         const user = localStorage.getItem("user_id");
@@ -13,32 +14,47 @@ const Purchased = () => {
         } else {
             console.log("Usuario no ha iniciado sesión");
         }
+
+        // Retrasar la aparición de la alerta después de 2 segundos (2000 ms)
+        const timer = setTimeout(() => {
+            setShowAlert(true);
+        }, 2000);
+
+        return () => clearTimeout(timer); // Limpiar el temporizador al desmontar el componente
     }, []);
 
     const fetchBoughtContent = async (user: string) => {
-    try {
-      const boughtContentData = await getBoughtContent(user);
-      setBoughtContent(boughtContentData);
-    } catch (error) {
-      console.log("Error al obtener el contenido comprado.");
-    }
-  };
+        try {
+            const boughtContentData = await getBoughtContent(user);
+            setBoughtContent(boughtContentData);
+        } catch (error) {
+            console.log("Error al obtener el contenido comprado.");
+        }
+    };
 
     return (
         <div>
-            {boughtContent.length === 0 && (
+            {showAlert && boughtContent.length === 0 && (
                 <div className="alert alert-warning mt-3 mb-4 mx-5" role="alert">
-                    ¡Bienvenid@! 👋  <br /> Parece que aún no has comprado ningún curso. <br />¡No te pierdas la oportunidad! 🚀🌟
+                    ¡Bienvenid@! 👋 <br /> Parece que aún no has comprado ningún curso.
+                    <br />
+                    ¡No te pierdas la oportunidad! 🚀🌟
                 </div>
             )}
             <div className="row">
                 {boughtContent.map((content) => (
-                    <div key={content._id} className="col-md-4 mb-4">
+                    <div key={content._id}>
                         <div style={{ cursor: "pointer" }}>
-                            <div className="card my-content-color-card my-content-card-50" >
+                            <div
+                                className="card my-content-color-card my-content-card-50"
+                            >
                                 <div className="card-body">
                                     <div className="content-header">
-                                        <Link to={`/studycontent`} className="custom-link" style={{ color: "black", textDecoration: "none" }}>
+                                        <Link
+                                            to={`/studycontent`}
+                                            className="custom-link"
+                                            style={{ color: "black", textDecoration: "none" }}
+                                        >
                                             <h1 className="card-title">{content.title}</h1>
                                         </Link>
                                     </div>
@@ -51,9 +67,5 @@ const Purchased = () => {
         </div>
     );
 };
+
 export default Purchased;
-
-
-
-
-
