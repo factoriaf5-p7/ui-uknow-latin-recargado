32b9ee465e1cd5c3ac51698e8176281cec3379b5
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-08-03 15:39:04 +0200

    update study

---
+++ u-know-front/src/components/StudyContent/StudyContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudyContent() {
+  return (
+    <div>StudyContent</div>
+  )
+}
--- u-know-front/src/pages/StudyContentPage.tsx
+++ u-know-front/src/pages/StudyContentPage.tsx
@@ -1,7 +1,9 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import StudyContent from "../components/StudyContent/StudyContent";
 
-export default function StudyContent() {
+
+export default function StudyContentPage() {
     return <>
         <Header />
         <StudyContent />
