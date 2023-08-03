74713b1aec7bd6e338e5a4a980748b80e16a1a37
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-08-03 15:18:16 +0200

    update study

--- u-know-front/src/pages/StudyContentPage.tsx
+++ u-know-front/src/pages/StudyContentPage.tsx
@@ -1,10 +1,10 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
-export default function RateContentPage() {
+export default function StudyContent() {
     return <>
         <Header />
-        
+        <StudyContent />
         <Footer />
     </>;
 }
