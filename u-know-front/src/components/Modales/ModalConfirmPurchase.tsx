cb041fa427d78cc2234c51323b38e8ee318fea23
tito <jfctito@gmail.com>
2023-08-03 20:34:11 +0200

    porfin comprar cursos funciona

--- u-know-front/src/components/Modales/ModalConfirmPurchase.tsx
+++ u-know-front/src/components/Modales/ModalConfirmPurchase.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Modal from 'react-modal'
-import { Course, buyContentUser } from '../../services/content.service'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import Modal from "react-modal";
+import { Course, buyContentUser } from "../../services/content.service";
+import { useNavigate } from "react-router-dom";
 
 interface ModalConfirmPurchaseProps {
-  isOpen: boolean
-  onRequestClose: () => void
-  courseId: Course
+  isOpen: boolean;
+  onRequestClose: () => void;
+  courseId: Course;
 }
 
 const ModalConfirmPurchase: React.FC<ModalConfirmPurchaseProps> = ({
@@ -14,36 +14,35 @@ const ModalConfirmPurchase: React.FC<ModalConfirmPurchaseProps> = ({
   onRequestClose,
   courseId,
 }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleConfirmPurchase = async () => {
-    const id: string | null = localStorage.getItem('user_id')
+    const id: string | null = localStorage.getItem("user_id");
+    if (id !== null) {
+      // aquí quiero usar el servicio buyContentUser pasandole el parametro "id"
+      // y el parametro "courseId._id"
+      try {
+        await buyContentUser(id, courseId._id);
 
-    // aquí quiero usar el servicio buyContentUser pasandole el parametro "id"
-    // y el parametro "courseId._id"
-    try {
-      await buyContentUser(id, courseId._id)
+        const walletBalanceStr: string | null =
+          localStorage.getItem("wallet_balance");
+        const walletBalance = Number(walletBalanceStr);
+        const updatedWalletBalance = walletBalance - courseId.price;
+        localStorage.setItem("wallet_balance", updatedWalletBalance.toString());
+        setTimeout(function () {
+          alert(
+            `Felicitaciones el curso ha sido comprado satisfactoriamente!!!`
+          );
+        }, 0);
+        navigate("/contentcart");
+      } catch (error) {
+        // Si hubo un error al realizar la compra, puedes manejarlo aquí
+        console.error(error);
+        onRequestClose();
 
-      const walletBalanceStr: string | null = localStorage.getItem(
-        'wallet_balance',
-      )
-      const walletBalance = Number(walletBalanceStr)
-      const updatedWalletBalance = walletBalance - courseId.price
-      localStorage.setItem('wallet_balance', updatedWalletBalance.toString())
-      setTimeout(function () {
-        alert(
-          `Felicitaciones el curso ha sido comprado satisfactoriamente!!!`,
-        )
-      }, 0)
-      navigate('/contentcart')
-    } catch (error) {
-      // Si hubo un error al realizar la compra, puedes manejarlo aquí
-      console.error(error)
-     onRequestClose()
-
-
-      // También puedes mostrar un mensaje de error o cualquier otra acción que desees
+        // También puedes mostrar un mensaje de error o cualquier otra acción que desees
+      }
     }
-  }
+  };
 
   return (
     <Modal
@@ -56,7 +55,7 @@ const ModalConfirmPurchase: React.FC<ModalConfirmPurchaseProps> = ({
       <button onClick={handleConfirmPurchase}>it's OK</button>
       <button onClick={onRequestClose}>Cancel</button>
     </Modal>
-  )
-}
+  );
+};
 
-export default ModalConfirmPurchase
+export default ModalConfirmPurchase;
